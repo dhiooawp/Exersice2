8b9951f3277623c53599891c9acb7000edc5d117
DhioAWP <dhioanugerah0@gmail.com>
2022-10-31 16:42:18 +0700

    add public void inpu pada class program

--- Exersice2/Program.cs
+++ Exersice2/Program.cs
@@ -6,5 +6,24 @@
         int n;
         int i;
 
+        public void input()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine("\nArray should have minimum 1 and maximum 20 elements.\n");
+            }
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
+        }
     }
 }
