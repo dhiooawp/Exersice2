db76367c64167cfada64c1108031329bf979a179
DhioAWP <dhioanugerah0@gmail.com>
2022-10-31 16:49:51 +0700

    add static void main

--- Exersice2/Program.cs
+++ Exersice2/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace BinarySeacrh
+﻿using System;
+
+
+namespace BinarySeacrh
 {
     class program
     {
@@ -24,6 +27,51 @@
                 string s1 = Console.ReadLine();
                 arr[i] = Int32.Parse(s1);
             }
+        }
+        public void BinarySeacrh()
+        {
+            char ch;
+            do
+            {
+                Console.Write("\nEnter element want you to search :");
+                int item = Convert.ToInt32(Console.ReadLine());
+
+                int lowerbound = 0;
+                int upperbound = n - 1;
+
+                int mid = (lowerbound + upperbound) / 2;
+                int ctr = 1;
+
+                while ((item != arr[mid]) && (lowerbound <= upperbound))
+                {
+                    if (item > arr[mid])
+                        lowerbound = mid + 1;
+                    else
+                        upperbound = mid - 1;
+                    mid = (lowerbound + upperbound) / 2;
+                    ctr++;
+                }
+                if (item == arr[mid])
+                    Console.WriteLine("\n" + item.ToString() + " found at position " + (mid + 1).ToString());
+                else
+                    Console.WriteLine("\n" + item.ToString() + " not found in the array\n");
+                Console.WriteLine("\nNumber of comparasion : " + ctr);
+
+                Console.Write("\nContinue search (y/n):");
+                ch = char.Parse(Console.ReadLine());
+
+            } while ((ch == 'y') || (ch == 'Y'));
+        }
+        static void Main(string[] args)
+        {
+            program mylist = new program();
+
+            mylist.input();
+
+            mylist.BinarySeacrh();
+
+            Console.WriteLine("\n\n Tekan tombol apa saja untuk keluar.");
+            Console.Read();
         }
     }
 }
